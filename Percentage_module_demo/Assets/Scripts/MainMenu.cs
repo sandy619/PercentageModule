ee4a18132d0a088a1652cd6337ec94d13098f930
Sandeep <sandeeppenkey@gmail.com>
2021-09-07 18:19:11 +0530

    First module done
    pizza module done

---
+++ Percentage_module_demo/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject mainmenu;
+    [SerializeField] GameObject objectives;
+    [SerializeField] GameObject learn01;
+    [SerializeField] GameObject learn02;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void selectObjectives()
+    {
+        mainmenu.SetActive(false);
+        objectives.SetActive(true);
+    }
+
+    public void selectLearn()
+    {
+        mainmenu.SetActive(false);
+        learn01.SetActive(true);
+    }
+
+    public void selectAssess()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Back00()
+    {
+        mainmenu.SetActive(true);
+        objectives.SetActive(false);
+        //learn01.SetActive(false);
+    }
+    public void Back01()
+    {
+        mainmenu.SetActive(true);
+        //objectives.SetActive(false);
+        learn01.SetActive(false);
+    }
+    public void Back02()
+    {
+        learn01.SetActive(true);
+        learn02.SetActive(false);
+    }
+    public void Next()
+    {
+        learn01.SetActive(false);
+        learn02.SetActive(true);
+    }
+
+}
---
+++ Percentage_module_demo/Assets/Scripts/Pizza.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Pizza : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float EnteredSlices;
+    [SerializeField] Text pizzaValue;
+    [SerializeField] Slider pizzaSlider;
+    [SerializeField] Text selectedSlices;
+    //[SerializeField] Text TotalSlices;
+    float totalslices = 10;
+    [SerializeField] Text percentage;
+    [SerializeField] Image pizzaTint;
+    [SerializeField] Image BarTint;
+    [SerializeField] Image PieTint;
+
+    [SerializeField] Text barpercent;
+    [SerializeField] Text piepercent;
+    void Start()
+    {
+        totalslices = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangePizzaSlider()
+    {
+        pizzaValue.text = Mathf.RoundToInt(pizzaSlider.value).ToString();
+        selectedSlices.text= Mathf.RoundToInt(pizzaSlider.value).ToString();
+        float percent = ((pizzaSlider.value) / totalslices)*100;
+        percentage.text = percent.ToString()+"%";
+        pizzaTint.fillAmount = pizzaSlider.value / 10;
+        PieTint.fillAmount = pizzaSlider.value / 10;
+        BarTint.fillAmount = pizzaSlider.value / 10;
+        barpercent.text = percent.ToString() + "%";
+        piepercent.text = percent.ToString() + "%";
+    }
+
+    public void OnNext()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Mainmenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
